c87b18db203ac70a5a30332967c3cb2f0a5a57fc
varunswing <varuns939@gmail.com>
2020-02-14 22:28:26 +0530

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,124 +1,87 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-void merge(int *,int, int , int );
-
-void merge_sort(int *arr, int low, int high)
+void Merge(int A[], int p, int q, int r)
 {
 
-    int mid;
-
-    if (low < high){
-
-        //divide the array at mid and sort independently using merge sort9
-        mid=(low+high)/2;
-
-        merge_sort(arr,low,mid);
-
-        merge_sort(arr,mid+1,high);
-
-        //merge or conquer sorted arrays
-
-        merge(arr,low,high,mid);
-
-    }
-
-}
-
-// Merge sort
-
-void merge(int *arr, int low, int high, int mid)
-
-{
-
-    int i, j, k, c[50];
-
-    i = low;
-
-    k = low;
-
-    j = mid + 1;
-
-    while (i <= mid && j <= high) {
-
-        if (arr[i] < arr[j]) {
-
-            c[k] = arr[i];
-
-            k++;
-
-            i++;
-
-        }
-
-        else  {
-
-            c[k] = arr[j];
-
-            k++;
-
-            j++;
-
-        }
-
-    }
-
-    while (i <= mid) {
-
-        c[k] = arr[i];
-
-        k++;
-
-        i++;
-
-    }
-
-    while (j <= high) {
-
-        c[k] = arr[j];
-
-        k++;
-
-        j++;
-
-    }
-
-    for (i = low; i < k; i++)  {
-
-        arr[i] = c[i];
-
-    }
-
-}
-
-// read input array and call mergesort
-
-int main()
-
-{
-
-    int myarray[30], num;
-
-    cout<<"Enter number of elements to be sorted:";
-
-    cin>>num;
-
-    cout<<"Enter "<<num<<" elements to be sorted:";
-
-    for (int i = 0; i < num; i++) { cin>>myarray[i];
-
-    }
-
-    merge_sort(myarray, 0, num-1);
-
-    cout<<"Sorted array\n";
-
-    for (int i = 0; i < num; i++)
-
+    int n1, n2, i, j, k;
+    //size of left array=n1
+    //size of right array=n2
+    n1 = q - p + 1;
+    n2 = r - q;
+    int L[n1], R[n2];
+    //initializing the value of Left part to L[]
+    for (i = 0; i < n1; i++)
     {
-
-        cout<<myarray[i]<<"\t";
-
+        L[i] = A[p + i];
     }
+    //initializing the value of Right Part to R[]
+    for (j = 0; j < n2; j++)
+    {
+        R[j] = A[q + j + 1];
+    }
+    i = 0, j = 0;
+    //Comparing and merging them
+    //into new array in sorted order
+    for (k = p; i < n1 && j < n2; k++)
+    {
+        if (L[i] < R[j])
+        {
+            A[k] = L[i++];
+        }
+        else
+        {
+            A[k] = R[j++];
+        }
+    }
+    //If Left Array L[] has more elements than Right Array R[]
+    //then it will put all the
+    // reamining elements of L[] into A[]
+    while (i < n1)
+    {
+        A[k++] = L[i++];
+    }
+    //If Right Array R[] has more elements than Left Array L[]
+    //then it will put all the
+    // reamining elements of L[] into A[]
+    while (j < n2)
+    {
+        A[k++] = R[j++];
+    }
+}
+//This is Divide Part
+//This part will Divide the array into
+//Sub array and then will Merge them
+//by calling Merge()
+void MergeSort(int A[], int p, int r)
+{
+    int q;
+    if (p < r)
+    {
+        q = (p + r) / 2;
+        MergeSort(A, p, q);
+        MergeSort(A, q + 1, r);
+        Merge(A, p, q, r);
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter size of the Array: ";
+    cin >> n;
+    int A[n], i;
+    cout << "Enter array values:\n";
+    for (i = 0; i < n; i++)
+        cin >> A[i];
+    //Calling the MergeSort()
+    //First we are passing the array
+    //the start index that is 0
+    //and the size of the array n
 
+    MergeSort(A, 0, n - 1);
+    cout << "The Sorted List is\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
+    return 0;
 }
